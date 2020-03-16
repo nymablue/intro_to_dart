02381c9d82eae7ddc739588131e7c3a8d99f146f
Brayan Mamani <61948537+BrayanMamani@users.noreply.github.com>
2020-03-16 10:05:14 -0400

    Optimized methods

--- bin/Bicycle.dart
+++ bin/Bicycle.dart
@@ -6,6 +6,10 @@ class Bicycle{
   int gear;
   //Constructor
   Bicycle(this.cadence, this.gear);
+  //Method applyBrake
+  void applyBrake(int decrement) => _speed -= decrement;
+  //Method speedUp
+  void speedUp(int increment) => _speed += increment;
   //Method toString
   @override
   String toString() =>'Bicycle: ${speed+1} mph';
