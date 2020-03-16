0393610afc73604ddbb19e390e6e83e5f3f16b55
Brayan Mamani <61948537+BrayanMamani@users.noreply.github.com>
2020-03-16 10:02:16 -0400

    Implementing methods

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,20 @@
-import 'Bicycle.dart';
-
-void main(){
-  var bike = Bicycle(2, 0, 1);
-  print(bike);
+class Bicycle{
+  //Attributes
+  int cadence;
+  int _speed = 0;
+  int get speed => _speed;
+  int gear;
+  //Constructor
+  Bicycle(this.cadence, this.gear);
+  //Method applyBrake
+  void applyBrake(int decrement){
+    _speed -= decrement;
+  }
+  //Method speedUp
+  void speedUp(int increment){
+    _speed += increment;
+  }
+  //Method toString
+  @override
+  String toString() =>'Bicycle: ${speed+1} mph';
 }
