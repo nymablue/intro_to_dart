8ea9852fab7668548c48167401144548b778702f
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-03-18 14:51:18 -0400

    Create a factory

---
+++ bin/factory/main.dart
@@ -0,0 +1,11 @@
+import 'circle.dart';
+import 'square.dart';
+
+void main(){
+  //Creating objects
+  final circle = Circle(2);
+  final square = Square(2);
+  //Showing objects
+  print(circle.area);
+  print(square.area);
+}
---
+++ bin/factory/shape.dart
@@ -0,0 +1,4 @@
+abstract class Shape{
+  //Method get area
+  num get area;
+}
