d20cac23a80e6ad966ba69a3a88510823d2e8f10
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-03-18 15:02:50 -0400

    Create a top-level function

--- bin/factory/main.dart
+++ bin/factory/main.dart
@@ -1,10 +1,18 @@
 import 'circle.dart';
+import 'shape.dart';
 import 'square.dart';
+
+//Top - level function
+Shape shapeFactory(String type){
+  if(type == 'circle') return Circle(2);
+  if(type == 'square') return Square(2);
+  throw 'can\'t create $type';
+}
 
 void main(){
   //Creating objects
-  final circle = Circle(2);
-  final square = Square(2);
+  final circle = shapeFactory('circle');
+  final square = shapeFactory('square');
   //Showing objects
   print(circle.area);
   print(square.area);
--- bin/factory/shape.dart
+++ bin/factory/shape.dart
@@ -1,3 +1,4 @@
+
 abstract class Shape{
   //Method get area
   num get area;
